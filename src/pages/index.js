e4172e28680af759b432ef3c51d1f49359279d98
Lucas Elias Baccan <naccabbaccan@hotmail.com>
2021-04-06 22:05:35 -0300

    Customizando pagina inicial

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,38 +8,35 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    title: <center>Projeto acadêmico</center>,
+    imageUrl: 'img/mochilaSmall.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Projeto desenvolvido para centralizar conteúdos apresentados na pós graduação.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    title: <center>Matérias e professores</center>,
+    imageUrl: 'img/documentSmall.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Para saber mais informações, acesse a área dos <a href="docs/">conteúdos</a> ou a área dos <a href="professores/">professores</a>
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: <center>Docusaurus: Powered by React</center>,
     imageUrl: 'img/undraw_docusaurus_react.svg',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        <a href="https://docusaurus.io/">Docusaurus</a>: Documentação feita de forma simples e rápida.
       </>
     ),
   },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
     <div className={clsx('col col--4', styles.feature)}>
@@ -56,25 +53,23 @@ function Feature({imageUrl, title, description}) {
 
 export default function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Projeto desenvolvido para centralizar conteúdos apresentados na pós graduação.">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
+          {/* Comentado o botão inicial, pode ser que depois utilizamos para algo */}
+          {/* <div className={styles.buttons}>
             <Link
-              className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
+              className={styles.indexCtasGetStartedButton}
               to={useBaseUrl('docs/')}>
               Get Started
             </Link>
-          </div>
+          </div> */}
         </div>
       </header>
       <main>
