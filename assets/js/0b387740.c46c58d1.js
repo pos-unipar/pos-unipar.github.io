bd83ed4c3250bede588cf874f76dd0d46b1b2786
lucasbaccan <naccabbaccan@hotmail.com>
2021-04-07 02:53:55 +0000

    Deploying to gh-pages from  @ 0e6f50e90b30582064c318db020c7b493b1b5c70 🚀

---
+++ assets/js/0b387740.c46c58d1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{71:function(o){o.exports=JSON.parse('{"pluginId":"docs","version":"current","label":"Next","isLast":true,"docsSidebars":{"docs":[{"collapsed":true,"type":"category","label":"Docusaurus Tutorial","items":[{"type":"link","label":"Todos os conte\xfados","href":"/docs/"}]}]},"permalinkToSidebar":{"/docs/":"docs"}}')}}]);
