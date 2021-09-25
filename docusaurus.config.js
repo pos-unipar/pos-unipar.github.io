ccffb386eaf5ca28d9f4cbdf4e2152d2988d1703
Lucas Elias Baccan <naccabbaccan@hotmail.com>
2021-09-25 12:16:01 -0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ module.exports = {
   themeConfig: {
     gtag: {
       // You can also use your "G-" Measurement ID here.
-      trackingID: 'G-JDYPJCC287',
+      trackingID: 'G-GL316DNCCY',
       // Optional fields.
       anonymizeIP: true, // Should IPs be anonymized?
     },
